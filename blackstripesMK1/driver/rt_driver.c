cbafc8a8544f9504ed56480d67af195f759f7b45
roysuter <roysuter@gmail.com>
2015-07-12 11:50:42 +0200

    Update rt_driver.c

--- blackstripesMK1/driver/rt_driver.c
+++ blackstripesMK1/driver/rt_driver.c
@@ -15,9 +15,9 @@ RT_TASK draw_task;
 #define LEFT_DIR RPI_GPIO_P1_07
 #define SOLENOID RPI_GPIO_P1_10
 
-//scp rt_driver.c pi@10.0.1.18:/home/jed/GPIO_C_driver
-//gcc -I/usr/xenomai/include -Ibcm2835-1.44/src bcm2835-1.44/src/bcm2835.c rt_driver.c -L/usr/xenomai/lib -lnative -lxenomai -o rt_driver
-//export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/xenomai/lib
+//scp rt_driver.c pi@10.0.1.18:/home/pi/GPIO_C_driver
+//gcc -o rt_driver -I bcm2835-1.44/src bcm2835-1.44/src/bcm2835.c rt_driver.c
+
 
 /* NOTE: error handling omitted. */
 
