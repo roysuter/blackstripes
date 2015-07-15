ae0660a862e8be6854c74b1d515e0b5021311051
roysuter <roysuter@gmail.com>
2015-07-15 15:23:03 +0200

    Update bcm2835.sh

--- blackstripesMK1/driver/bcm2835.sh
+++ blackstripesMK1/driver/bcm2835.sh
@@ -1,5 +1,6 @@
 #//install bcm2835 V1.44
 #// The library homepage  http://www.airspayce.com/mikem/bcm2835/
+#//scp bcm2835.sh pi@10.0.1.18:/home/pi/GPIO_C_driver
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.44.tar.gz
 tar zxvf bcm2835-1.44.tar.gz
 cd bcm2835-1.44
