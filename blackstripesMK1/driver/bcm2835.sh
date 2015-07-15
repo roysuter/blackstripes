7d790cc491a432133c408859b86ebeeb1936354f
roysuter <roysuter@gmail.com>
2015-07-15 15:03:24 +0200

    Update bcm2835.sh

--- blackstripesMK1/driver/bcm2835.sh
+++ blackstripesMK1/driver/bcm2835.sh
@@ -1,8 +1,8 @@
-//install bcm2835 V1.88
+//install bcm2835 V1.44
 // The library homepage  http://www.airspayce.com/mikem/bcm2835/
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.88.tar.gz
-tar zxvf bcm2835-1.88.tar.gz
-cd bcm2835-1.88
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.44.tar.gz
+tar zxvf bcm2835-1.44.tar.gz
+cd bcm2835-1.44
 ./configure
 sudo make check
 sudo make install
