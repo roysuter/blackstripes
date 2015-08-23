adb5bd5dd58c079df29b4457113af1c388c0c886
roysuter <roysuter@gmail.com>
2015-08-23 09:55:52 +0200

    Update markerdown.c

--- blackstripesMK1/driver/markerdown.c
+++ blackstripesMK1/driver/markerdown.c
@@ -7,8 +7,8 @@
 //gcc -o markerdown -I bcm2835-1.44/src bcm2835-1.44/src/bcm2835.c markerdown.c
 
 
-#define RIGHT_CLOCK RPI_GPIO_P1_05
-#define RIGHT_DIR RPI_GPIO_P1_03
+#define RIGHT_CLOCK RPI_GPIO_P1_12
+#define RIGHT_DIR RPI_GPIO_P1_11
 #define LEFT_CLOCK RPI_GPIO_P1_08
 #define LEFT_DIR RPI_GPIO_P1_07
 #define SOLENOID RPI_GPIO_P1_10
