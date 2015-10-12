784e06bc48f1aae70f25847e5e8ec5b72d414e7d
roysuter <roysuter@gmail.com>
2015-10-12 23:24:39 +0200

    Update server.py

--- blackstripesMK1/driver/server.py
+++ blackstripesMK1/driver/server.py
@@ -6,7 +6,10 @@ import tornado.websocket
 
 # http://niltoid.com/blog/raspberry-pi-arduino-tornado/
 # Adding websockets to your Tornado app is easy. Below is the modified version of server.py. It also includes support for command line arguments on line 8 (so you can run the program using “python server.py –port=8080″).
-# 
+# And here is the corresponding index.html, which connects to the websocket server. I used KineticJS to quickly create a simple red circle on a canvas which listens from click events.  Be sure that your “ws://” url is correct.
+#
+#
+#
  
 from tornado.options import define, options
 define("port", default=8080, help="run on the given port", type=int)
