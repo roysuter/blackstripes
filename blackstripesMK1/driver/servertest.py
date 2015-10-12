c11cf9a26ae2da230954ca2b3441d56e88300f69
roysuter <roysuter@gmail.com>
2015-10-12 23:26:13 +0200

    Update servertest.py

--- blackstripesMK1/driver/servertest.py
+++ blackstripesMK1/driver/servertest.py
@@ -1,5 +1,9 @@
 import tornado.ioloop
 import tornado.web
+
+#
+# To make sure everything works, create a file named “server.py” containing the following code (taken from Tornado’s site), and run it from terminal using the command “python server.py”
+#
  
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
